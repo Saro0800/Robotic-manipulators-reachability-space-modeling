43627a2b1b9e436c1f784705f032ba7c0c647d4b
Rosario <rosario.cavelli@icloud.com>
2025-01-13 17:32:30 +0100

    Added the section regarding the computation of the ellipsoid's parameters using the optimization problem approach to the README.md, together with the relative script files and some images.

--- README.md
+++ README.md
@@ -26,13 +26,13 @@ The use of this script alone is intended for visualization purposes only. Before
 The center of the code is represented by the GUI showed below.
 
 <div align="center">
-    <img src="images/GUI_img.jpg" alt="Esempio di immagine" width="600">
+    <img src="images/GUI_img.jpg" width="600">
 </div>
 
 It is possible to identify a total of 6 sections, as highlighted in the following picture.
 
 <div align="center">
-    <img src="images/GUI_img_commented.jpg" alt="Esempio di immagine" width="600">
+    <img src="images/GUI_img_commented.jpg" width="600">
 </div>
 
 In more details:
@@ -55,7 +55,7 @@ As an example, the point cloud obtained for the ur5e manipulator from the Univer
 Please note that, as cited above, using the *gen_cloud_GUI.py* script will result only in the computation of the point cloud and eventually its visualization. As a result, the ellipsoid is not visible, since its parameters are not computed.
 
 <div align="center">
-    <img src="images/ur5e_pointcloud.png" alt="Esempio di immagine" width="1000">
+    <img src="images/ur5e_pointcloud.png" width="1000">
 </div>
 
 
@@ -64,12 +64,46 @@ Please note that, as cited above, using the *gen_cloud_GUI.py* script will resul
 
 
 
-
-
-
-
 ## Optimization problem
-This tool allow to obtain the parameters of the ellipsoid equation fallowing a minimization fashion.
+The first way to compute the parameters of the ellipsoid is by solving a proper optimization problem. For more details about the design of the optimization problem please refere to the paper.
+
+All the files needed to obtain the equation of the ellipsoid enclosing the reachability space are contained in the ***opt_problem*** folder.
+
+### Problem definition
+The ***problem_formulation.py*** file contains the definition of a class where the optimization problem is defined, named **EllipsoidEquationOptProblem**, by extending the *ElementwiseProblem* class of the *pymoo* library.
+
+When an object of this class is instantiated, a set of few operations are completed, as specified in the *\__init__* method. In order:
+1. the points building the point cloud of the desired manipulator are retrieved;
+2. if not provided, an estimation of the center of the point cloud as the mean point is computed;
+3. the parameters of the optimization problem are set. More in details, the number of optimization variables, the number of objective functions, the inequality contraints, the lower and upper bounds of the optimization variables.
+
+A second method, namely *_evaluate*, is defined. It is the function that is called at every iteration of the optimization problem.  It retrieves the current solution and computes the values of the objective function until now.
+
+### Usage
+It is possible to obtain the equation of the ellipsoid by running the script *eqn_solv_opt.py*:
+```
+python3 opt_problem/eqn_solv_opt.py
+```
+This script is based on the GUI described above to select the URDF of the robot, to select the important joints, the number of samples per joint and, finally, to genereate the point cloud exploited by the optimization problem.
+
+It is important to notice that the parameters characterizing the equation of the ellipsoid are computed as soon as the point cloud is obtained and after the GUI has been closed.
+
+It is possible to select the desired optimization algoritm by changing the value of the variable *alg_name*. The possible strings are:
+* ***"PatternSearch"*** to run the optimization problem using the Pattern Search algorithm;
+* ***"GA"*** to run the optimization problem exploiting the Genetic Algorithm;
+* ***"PSO"*** to run the optimization problem using the Particle Swarm Optimization Algorithm.
+
+Once the optimization problem is solved, the parameters of the best solution found are printed on the screen. In addition to that, 2 ROS messages are created and published:
+1. A ***Marker*** message is published by the *reachability_pointcloud_publisher* node (initialized when the GUI is created) on a topic named ***/vis_reachability_ellipsoid***. The marker's position and the lenght of its axes are equal to the parameters characterizing the ellipsoid equation. In this way, a visualization of the computed equation can be obtained and displayed in Rviz.
+
+2. A ***PointCloud2*** message is published by the *reachability_pointcloud_publisher* node on a topic named ***/vis_ellipsoid_center***. It is used to show the center of the ellipsoid.
+
+### Example
+As an example, the ellipsoid (in green) enclosing the reachability space of the LoCoBot WX250s from Trossen Robotics (whose URDF can be found in the *generate_pointcloud/models* folder) is visualized in Rviz along with the point cloud (in red).
+
+<div align="center">
+    <img src="images/locobot_ellipsoid.png" width="800">
+</div>
 
 
 
---
+++ opt_problem/eqn_solv_opt.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python3
+import sys
+sys.path.append("..")
+
+import numpy as np
+import rospy
+import math
+import time
+
+from opt_problem.problem_formulation import EllipsoidEquationOptProblem
+from generate_pointcloud.gen_cloud_GUI import GenereatePointCloud
+
+from pymoo.core.population import Population
+from pymoo.algorithms.soo.nonconvex.pattern import PatternSearch
+from pymoo.algorithms.soo.nonconvex.ga import GA
+from pymoo.algorithms.soo.nonconvex.pso import PSO
+from pymoo.optimize import minimize
+from pymoo.termination.robust import RobustTermination
+from pymoo.termination.ftol import SingleObjectiveSpaceTermination
+
+from visualization_msgs.msg import Marker
+from sensor_msgs.msg import PointCloud2, PointField
+from std_msgs.msg import Header
+from sensor_msgs.point_cloud2 import create_cloud
+
+
+def solve_eqn_prob(points, alg_name, link=None, center=None, viz_res=False):
+    # define the problem
+    problem = EllipsoidEquationOptProblem(center, points, viz_res, None, None)
+        
+    # define the weights of the points and of the volume, and the optimization algorithm
+    if alg_name=="PatternSearch":
+        problem.num_points_wt = 1
+        problem.volume_wt = pow(10, int(math.log10(points.shape[0])-1))
+
+        x0 = np.array([0.1, 0.1, 0.1, problem.center[0], problem.center[1], problem.center[2]])
+        algorithm = PatternSearch(
+                                delta=0.1,
+                                rho=0.1,
+                                step_size=0.1,
+                                x0 = x0)
+    
+    elif alg_name=="GA":
+        problem.num_points_wt = 1
+        problem.volume_wt = pow(10, int(math.log10(points.shape[0])-1))
+
+        x0 = np.array([[1, 1, 1, problem.center[0], problem.center[1], problem.center[2]]])
+        pop = Population.new(X=x0)
+        algorithm = GA(sampling=pop)
+    
+    
+    elif alg_name=="PSO":
+        problem.num_points_wt = 1
+        problem.volume_wt = 1
+
+        algorithm = PSO()
+
+
+    termination = RobustTermination(
+                        SingleObjectiveSpaceTermination(tol=pow(10,-6))     
+                    )
+
+    # solve the optimization problem
+    res = minimize(problem=problem,
+                   algorithm=algorithm,
+                   termination=termination,
+                   verbose = False,
+                   seed = 1)
+    
+    if viz_res==True:
+        print("Best solution found: \n\ta={:.4f}, b={:.4f}, c={:.4f}\n\txC={:.4f}, yC={:.4f}, zC={:.4f}".format(res.X[0], res.X[1], res.X[2], res.X[3], res.X[4], res.X[5]))
+    return res
+
+def create_marker_msg(points, link, res, center = None):
+    # retrieve the solution of the opt problem
+    a = res.X[0]
+    b = res.X[1]
+    c = res.X[2]
+
+    # compute the center
+    if center is None:
+        center = np.array([np.mean([np.min(points[:,0]), np.max(points[:,0])]),
+                            np.mean([np.min(points[:,1]), np.max(points[:,1])]),
+                            np.mean([np.min(points[:,2]), np.max(points[:,2])])])
+    
+    marker_msg = Marker()
+    marker_msg.header.frame_id = link
+    marker_msg.header.stamp = rospy.Time.now()
+
+    # set the shape
+    marker_msg.type = 2
+    marker_msg.id = 0
+
+    # set the scale of the marker
+    marker_msg.scale.x = 2*a
+    marker_msg.scale.y = 2*b
+    marker_msg.scale.z = 2*c
+
+    # set the color
+    marker_msg.color.r = 0.0
+    marker_msg.color.g = 1.0
+    marker_msg.color.b = 0.0
+    marker_msg.color.a = 0.5
+
+    # set the pose of the marker
+    marker_msg.pose.position.x = center[0]
+    marker_msg.pose.position.y = center[1]
+    marker_msg.pose.position.z = center[2]
+    marker_msg.pose.orientation.x = 0.0
+    marker_msg.pose.orientation.y = 0.0
+    marker_msg.pose.orientation.z = 0.0
+    marker_msg.pose.orientation.w = 1.0
+
+    return marker_msg
+
+def create_center_msg(center, link):
+
+        header = Header()
+        header.stamp = rospy.Time.now()
+        header.frame_id = link
+
+        fields = [
+            PointField('x', 0, PointField.FLOAT32, 1),
+            PointField('y', 4, PointField.FLOAT32, 1),
+            PointField('z', 8, PointField.FLOAT32, 1),
+        ]
+
+        pointcloud_msg = create_cloud(header, fields, (center,))
+
+        return pointcloud_msg
+
+
+if __name__=="__main__":
+    # get the pointcloud points
+    gen_cloud = GenereatePointCloud()
+    gen_cloud.create_ros_node()
+    gen_cloud.create_GUI()
+    gen_cloud.from_extern = True
+
+    # rospy.init_node('reachability_ellipsoid_publisher', anonymous=True)
+    pub_ellipsoids = rospy.Publisher('/vis_reachability_ellipsoid', Marker, queue_size=10)
+    pub_center = rospy.Publisher('/viz_ellipsoid_center', PointCloud2, queue_size=10)
+    rate = rospy.Rate(1)  # 1 Hz
+
+    # set the number of sample
+    gen_cloud.num_samples = 20
+
+    # create the point cloud
+    gen_cloud.generate_point_cloud()
+
+    # solve the optimization problem
+    points = gen_cloud.points
+    link = gen_cloud.point_cloud_orig_frame
+    center = np.array([np.mean([np.min(points[:,0]), np.max(points[:,0])]),
+                       np.mean([np.min(points[:,1]), np.max(points[:,1])]),
+                       np.mean([np.min(points[:,2]), np.max(points[:,2])])])
+    
+    start = time.time()
+
+    # alg_name = "PatternSearch"
+    # alg_name = "GA"
+    alg_name = "PSO"
+    res = solve_eqn_prob(points, alg_name, link, center, viz_res=True)
+    
+    end = time.time() - start
+    print("Solution found in {:.4f}s".format(end))
+    
+    center = np.array([res.X[3], res.X[4], res.X[5]])
+
+    # create the marker msg
+    marker_msg = create_marker_msg(points, link, res, center)
+
+    # create the center message
+    center_msg = create_center_msg(center, link)
+
+    pub_cloud = gen_cloud.pub_points
+    cloud_msg = gen_cloud.pointcloud_msg
+
+
+    print("Start publishing message")
+    while not rospy.is_shutdown():
+        pub_ellipsoids.publish(marker_msg)
+        rate.sleep()
+        pub_center.publish(center_msg)
+        rate.sleep()
+        pub_cloud.publish(cloud_msg)
+        rate.sleep()
+
